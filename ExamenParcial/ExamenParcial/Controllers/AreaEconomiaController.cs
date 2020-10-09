8a0dac0bd7341cf5caf406560780368c9a96758c
antonevj <antonevj26@gmail.com>
2020-10-08 23:16:55 -0500

    creacions de los interes en el controlador

--- ExamenParcial/ExamenParcial/Controllers/AreaEconomiaController.cs
+++ ExamenParcial/ExamenParcial/Controllers/AreaEconomiaController.cs
@@ -14,5 +14,41 @@ namespace ExamenParcial.Controllers
             return View();
 
         }
+
+        [HttpGet]
+        public ViewResult interes()
+
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult interes(float capital, float tasa, float tiempo, string inte)
+        {
+            float rpta;
+
+            if (inte=="simple")
+            {
+                rpta = (float)(capital * (tasa / 100) * tiempo);
+            }
+            else
+            {
+                rpta = (float)(capital * (1+(tasa / 100)) * tiempo);
+            }
+              
+
+
+
+            ViewBag.rpta = rpta;
+            ViewBag.capital = capital;
+            ViewBag.tasa = tasa;
+            ViewBag.inte = inte;
+            ViewBag.tiempo = tiempo;
+           
+            return View();
+
+
+        }
+
     }
 }
